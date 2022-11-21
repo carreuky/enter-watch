c615d247203edad92d3d22936950b5c65b4be186
carreuky <carreuky@gmail.com>
2022-11-22 02:03:00 +0300

    trending card

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
+import MainContainer from './components/MainContainer';
 import Sidebar from './components/Sidebar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-dark">
+    <div className="bg-dark lg:ml-24 lg:mr-4 lg:mt-8  font-sans">
       <BrowserRouter>
       <Sidebar/>
       <Routes>
-      
+      <Route path="/" element={<MainContainer/>} />
+
       </Routes>
     </BrowserRouter>
    
---
+++ src/components/Trending.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import TrendingCrad from './TrendingCrad'
+
+export default function Trending() {
+  return (
+    <div><h1 className='text-4xl'>Trending</h1>
+    <TrendingCrad/>
+    </div>
+  )
+}
